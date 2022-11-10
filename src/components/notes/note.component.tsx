8b7bf51bc008765d3811e466dbb0b96eeec3142b
Edem Ziddah <contact@codevoweb.com>
2022-11-10 20:13:45 +0000

    updated

--- src/components/notes/note.component.tsx
+++ src/components/notes/note.component.tsx
@@ -1,13 +1,13 @@
-import React, { FC, useEffect, useState } from 'react';
-import { format, parseISO } from 'date-fns';
-import { twMerge } from 'tailwind-merge';
-import NoteModal from '../note.modal';
-import UpdateNote from './update.note';
-import { toast } from 'react-toastify';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import NProgress from 'nprogress';
-import { INote } from '../../api/types';
-import { deleteNoteFn } from '../../api/noteApi';
+import React, { FC, useEffect, useState } from "react";
+import { format, parseISO } from "date-fns";
+import { twMerge } from "tailwind-merge";
+import NoteModal from "../note.modal";
+import UpdateNote from "./update.note";
+import { toast } from "react-toastify";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import NProgress from "nprogress";
+import { INote } from "../../api/types";
+import { deleteNoteFn } from "../../api/noteApi";
 
 type NoteItemProps = {
   note: INote;
@@ -24,10 +24,10 @@ const NoteItem: FC<NoteItemProps> = ({ note }) => {
       NProgress.start();
     },
     onSuccess(data) {
-      queryClient.invalidateQueries(['getNotes']);
-      toast('Note deleted successfully', {
-        type: 'success',
-        position: 'top-right',
+      queryClient.invalidateQueries(["getNotes"]);
+      toast("Note deleted successfully", {
+        type: "warning",
+        position: "top-right",
       });
       NProgress.done();
     },
@@ -39,68 +39,68 @@ const NoteItem: FC<NoteItemProps> = ({ note }) => {
         error.message ||
         error.toString();
       toast(resMessage, {
-        type: 'error',
-        position: 'top-right',
+        type: "error",
+        position: "top-right",
       });
       NProgress.done();
     },
   });
 
   const onDeleteHandler = (noteId: string) => {
-    if (window.confirm('Are you sure')) {
+    if (window.confirm("Are you sure")) {
       deleteNote(noteId);
     }
   };
   return (
     <>
-      <div className='p-4 bg-white rounded-lg border border-gray-200 shadow-md flex flex-col justify-between'>
-        <div className='details'>
-          <h4 className='mb-2 pb-2 text-2xl font-semibold tracking-tight text-gray-900'>
+      <div className="p-4 bg-white rounded-lg border border-gray-200 shadow-md flex flex-col justify-between overflow-hidden">
+        <div className="details">
+          <h4 className="mb-2 pb-2 text-2xl font-semibold tracking-tight text-gray-900">
             {note.title.length > 40
-              ? note.title.substring(0, 40) + '...'
+              ? note.title.substring(0, 40) + "..."
               : note.title}
           </h4>
-          <p className='mb-3 font-normal text-ct-dark-200'>
+          <p className="mb-3 font-normal text-ct-dark-200">
             {note.content.length > 210
-              ? note.content.substring(0, 210) + '...'
+              ? note.content.substring(0, 210) + "..."
               : note.content}
           </p>
         </div>
-        <div className='relative border-t border-slate-300 flex justify-between items-center'>
-          <span className='text-ct-dark-100 text-sm'>
-            {format(parseISO(String(note.createdAt)), 'PPP')}
+        <div className="relative border-t border-slate-300 flex justify-between items-center">
+          <span className="text-ct-dark-100 text-sm">
+            {format(parseISO(String(note.createdAt)), "PPP")}
           </span>
           <div
             onClick={() => setOpenSettings(!openSettings)}
-            className='text-ct-dark-100 text-lg cursor-pointer'
+            className="text-ct-dark-100 text-lg cursor-pointer"
           >
-            <i className='bx bx-dots-horizontal-rounded'></i>
+            <i className="bx bx-dots-horizontal-rounded"></i>
           </div>
           <div
-            id='settings-dropdown'
+            id="settings-dropdown"
             className={twMerge(
               `absolute right-0 bottom-3 z-10 w-28 text-base list-none bg-white rounded divide-y divide-gray-100 shadow`,
-              `${openSettings ? 'block' : 'hidden'}`
+              `${openSettings ? "block" : "hidden"}`
             )}
           >
-            <ul className='py-1' aria-labelledby='dropdownButton'>
+            <ul className="py-1" aria-labelledby="dropdownButton">
               <li
                 onClick={() => {
                   setOpenSettings(false);
                   setOpenNoteModal(true);
                 }}
-                className='py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer'
+                className="py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
               >
-                <i className='bx bx-pencil'></i> Edit
+                <i className="bx bx-pencil"></i> Edit
               </li>
               <li
                 onClick={() => {
                   setOpenSettings(false);
                   onDeleteHandler(note.id);
                 }}
-                className='py-2 px-4 text-sm text-red-600 hover:bg-gray-100 cursor-pointer'
+                className="py-2 px-4 text-sm text-red-600 hover:bg-gray-100 cursor-pointer"
               >
-                <i className='bx bx-trash'></i> Delete
+                <i className="bx bx-trash"></i> Delete
               </li>
             </ul>
           </div>
